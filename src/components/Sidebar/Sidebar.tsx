84f7e77cfeba411f9db758f3920528cd65f4a706
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-08-03 00:39:47 -0300

    Updated Sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,12 +1,22 @@
+import { useContext } from "react";
+import DataContext, { selectionOptions } from "../../contexts/data.context";
+
 import "./sidebar.scss";
 
 const Sidebar = () => {
+  const { setSelected } = useContext(DataContext);
+
+  const handleClick = (buttonClicked: selectionOptions, e: MouseEvent) => {
+    setSelected(buttonClicked);
+    
+  }
+
   return (
     <aside className="sidebar">
       <div className="sidebar__options">
-        <a href="#" className="sidebar__option">Cadastros</a>
-        <a href="#" className="sidebar__option">Visitantes</a>
-        <a href="#" className="sidebar__option">Registros</a>
+        <button className="sidebar__option" onClick={(e) => handleClick("users", e)}>Cadastros</button>
+        <button className="sidebar__option" onClick={(e) => handleClick("visitors", e)}>Visitantes</button>
+        <button className="sidebar__option" onClick={(e) => handleClick("history", e)}>Histórico de visitas</button>
       </div>
     </aside>
   );
