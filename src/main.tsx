ac576461b953dd6cd28d5271292b05a6fedd9079
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-08-03 00:39:33 -0300

    DataProvider configured

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { DataProvider } from "./contexts/data.context";
 import "./styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <DataProvider>
+      <App />
+    </DataProvider>
   </React.StrictMode>
 );
