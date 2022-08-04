cd6d246e43f5e1e803449f6d6b3523855a1c2d5d
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-08-03 23:41:59 -0300

    Sidebar option selection configured

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 import DataContext, { selectionOptions } from "../../contexts/data.context";
 
 import "./sidebar.scss";
@@ -6,15 +6,25 @@ import "./sidebar.scss";
 const Sidebar = () => {
   const { setSelected } = useContext(DataContext);
 
-  const handleClick = (buttonClicked: selectionOptions, e: MouseEvent) => {
+  const handleClick = (buttonClicked: selectionOptions, e: React.MouseEvent<HTMLButtonElement>) => {
     setSelected(buttonClicked);
+    
+    const typeAssertion = e.target as HTMLElement;
+    const options = typeAssertion.parentElement?.children;
+    
+    if (!options) return;    
+    const optionsArray = Array.from(options);
+    
+    optionsArray.map(option => option.classList.remove("sidebar__selected"));
+    const buttonToAddClassName = optionsArray.find(option => option === e.target);
+    buttonToAddClassName?.classList.add("sidebar__selected");
     
   }
 
   return (
     <aside className="sidebar">
       <div className="sidebar__options">
-        <button className="sidebar__option" onClick={(e) => handleClick("users", e)}>Cadastros</button>
+        <button className="sidebar__option sidebar__selected" onClick={(e) => handleClick("users", e)}>Cadastros</button>
         <button className="sidebar__option" onClick={(e) => handleClick("visitors", e)}>Visitantes</button>
         <button className="sidebar__option" onClick={(e) => handleClick("history", e)}>Histórico de visitas</button>
       </div>
