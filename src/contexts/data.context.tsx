83a90f28835479c92ab44ff75e8fd4acb3927ae9
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-08-05 12:48:18 -0300

    added isModalOpen useState

--- src/contexts/data.context.tsx
+++ src/contexts/data.context.tsx
@@ -3,6 +3,8 @@ import { createContext, ReactNode, useState } from "react";
 interface ContextType {
   selected: null |"users" | "visitors" | "history";
   setSelected: (buttonClicked: selectionOptions) => void;
+  isModalOpen: boolean;
+  setIsModalOpen: (isOpen: boolean) => void;
 }
 
 const DataContext = createContext({} as ContextType);
@@ -16,11 +18,14 @@ export type selectionOptions = null |"users" | "visitors" | "history";
 
 export const DataProvider = ({ children }: DataProvider) => {
   const [selected, setSelected] = useState<selectionOptions>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <DataContext.Provider value={{
       selected,
-      setSelected
+      setSelected,
+      isModalOpen,
+      setIsModalOpen
     }}>
       { children }
     </DataContext.Provider>
