667218e523344dd6083ef933b4ac7974bea57437
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-08-06 20:58:43 -0300

    Updated UserCard w/ variable data

--- src/components/Cards/UserCard.tsx
+++ src/components/Cards/UserCard.tsx
@@ -1,30 +1,52 @@
+import { formatCPF } from "../../utils/formatCPF";
 import "./card.scss";
 
-const UserCard = () => {
+interface UserCardProps {
+  userPicture?: string;
+  nome: string;
+  cpf: number;
+  cidade?: string;
+  bairro?: string;
+  rua?: string;
+  numero?: string;
+  complemento?: string;
+}
+
+const UserCard = ({
+  userPicture,
+  nome,
+  cpf,
+  cidade,
+  bairro,
+  rua,
+  numero,
+  complemento,
+}: UserCardProps) => {
+
   return (
     <button className="card">
       <img
         className="card__image"
-        src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+        src={userPicture}
         alt="imagem aleatória"
       />
       <div className="card__section">
         <span className="card__label">nome</span>
-        <p className="card__info">André Aquino de Bragança</p>
+        <p className="card__info">{nome}</p>
       </div>
-      
+
       <div className="card__vertical-line" />
-      
+
       <div className="card__section">
         <span className="card__label">cpf</span>
-        <p className="card__info">123.456.789-0</p>
+        <p className="card__info">{formatCPF(cpf)}</p>
       </div>
 
       <div className="card__vertical-line" />
 
       <div className="card__section">
         <span className="card__label">endereço</span>
-        <p className="card__info">Av. Alameda, 222 — Fortaleza</p>
+        <p className="card__info">{`${cidade} – ${rua}, ${numero}`}</p>
       </div>
     </button>
   );
