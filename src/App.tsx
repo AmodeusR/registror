30e02be91d126ae0ff96f885989021676e633302
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-08-09 18:43:02 -0300

    Configured base UserInfoCardu sage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,46 @@
-import { Header, Sidebar, Main } from "./components";
-
+import { useContext, useEffect, useState } from "react";
+import { Header, Sidebar, Main, UserInfoCard } from "./components";
+import { UserCardProps } from "./components/Cards/cards.types";
+import DataContext from "./contexts/data.context";
 
 function App() {
+  const [user, setUser] = useState({} as UserCardProps);
+
+  const { userCardInfoModalId } = useContext(DataContext);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const usersPromise = await fetch("http://localhost:3000/users");
+      const users = await usersPromise.json();
+      
+      const selectedUser = users.find((user: UserCardProps) => user.cpf === Number(userCardInfoModalId));
+
+      setUser(selectedUser);
+      console.log(selectedUser);
+      
+
+    }
+
+    fetchUser();
+  }, [userCardInfoModalId]);
   return (
     <div className="app">
       <Header />
       <div className="main-wrapper">
         <Sidebar />
         <Main />
+        {userCardInfoModalId && user &&
+         <UserInfoCard
+          nome={user.nome}
+          cpf={user.cpf}
+          cidade={user.cidade}
+          bairro={user.bairro}
+          rua={user.rua}
+          numero={user.numero}
+          complemento={user.complemento}
+          userPicture={user.userPicture}
+        />
+        }
       </div>
     </div>
   );
