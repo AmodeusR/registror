c0c4e816fb46c7eb32e5f9503b4e573a112a0517
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-08-09 18:41:21 -0300

    Card types

---
+++ src/components/Cards/cards.types.ts
@@ -0,0 +1,10 @@
+export interface UserCardProps {
+  userPicture?: string;
+  nome: string;
+  cpf: number;
+  cidade?: string;
+  bairro?: string;
+  rua?: string;
+  numero?: string;
+  complemento?: string;
+}
