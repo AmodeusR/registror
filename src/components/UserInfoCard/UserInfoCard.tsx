a9b532e86a8855a28f96c2aae2946e615733ed01
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-08-09 18:33:53 -0300

    Created UserInfoCard component

---
+++ src/components/UserInfoCard/UserInfoCard.tsx
@@ -0,0 +1,85 @@
+import { useContext, MouseEvent } from "react";
+import DataContext from "../../contexts/data.context";
+import { X } from "phosphor-react";
+import { UserCardProps } from "../Cards/cards.types";
+
+import "./userinfo.scss";
+import { formatCPF } from "../../utils/formatCPF";
+
+const UserInfoCard = ({
+  userPicture,
+  nome,
+  cpf,
+  cidade,
+  bairro,
+  rua,
+  numero,
+  complemento,
+}: UserCardProps) => {
+  const { setUserCardInfoModalId } = useContext(DataContext);
+
+  const handleModalClose = (e: MouseEvent) => {
+    const target = e.target as HTMLElement;
+
+    if (target.tagName === "DIV") {
+      setUserCardInfoModalId("");
+    }
+  };
+
+  return (
+    <div
+      className="create-user-background"
+      onClick={(e) => handleModalClose(e)}
+    >
+        <div className="userinfo">
+          <X
+            weight="bold"
+            size={32}
+            className="create-user__close-button"
+            onClick={() => setUserCardInfoModalId("")}
+          />
+          <img className="userinfo__image" src={userPicture || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"} alt="" />
+          <span className="create-user__section-title">Dados pessoais</span>
+          
+          <div className="userinfo__info">
+            <h2 className="userinfo__title">Nome</h2>
+            <p className="userinfo__description">{nome}</p>
+          </div>
+          <div className="userinfo__info">
+            <h2 className="userinfo__title">CPF</h2>
+            <p className="userinfo__description">{formatCPF(cpf)}</p>
+          </div>
+
+          <span className="create-user__section-title">Endereço</span>
+          
+          <div className="userinfo__info">
+            <h2 className="userinfo__title">Cidade</h2>
+            <p className="userinfo__description">{cidade || "Não informado"}</p>
+          </div>
+          <div className="userinfo__info">
+            <h2 className="userinfo__title">Bairro</h2>
+            <p className="userinfo__description">{bairro || "Não informado"}</p>
+          </div>
+          <div className="userinfo__flex">
+            <div className="userinfo__info">
+              <h2 className="userinfo__title">Rua</h2>
+              <p className="userinfo__description">{rua || "Não informado"}</p>
+            </div>
+            <div className="userinfo__info">
+              <h2 className="userinfo__title">Número</h2>
+              <p className="userinfo__description">{numero || "Não informado"}</p>
+            </div>
+          </div>
+          <div className="userinfo__info">
+            <h2 className="userinfo__title">Complemento</h2>
+            <p className="userinfo__description">{complemento || "Não informado"}</p>
+          </div>
+          <button type="button" className="create-user__button">
+            Registrar visita
+          </button>
+        </div>
+    </div>
+  );
+};
+
+export default UserInfoCard;
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Sidebar } from "./Sidebar/Sidebar";
 export { default as Main } from "./Main/Main"; 
 export { default as CreateUser } from "./CreateUser/CreateUser"; 
 export { default as InputLabel } from "./InputLabel/InputLabel"; 
+export { default as UserInfoCard } from "./UserInfoCard/UserInfoCard"; 
