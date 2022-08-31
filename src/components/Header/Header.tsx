20c28f2c4870fd84bcb7b3439419cee88d8700dd
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-08-31 19:43:08 -0300

    Updated Header with User data and sign out option

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,23 @@
 
 import { useContext, useEffect, useRef } from "react";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 import DataContext from "../../contexts/data.context";
+import { userSignOut } from "../../utils/firebase";
 import CreateGuest from "../CreateGuest/CreateGuest";
 import "./header.scss";
 
 
 const Header = () => {
   const input = useRef<HTMLInputElement>(null);
-  const { isRegisterModalOpen, setIsRegisterModalOpen, search, setSearch } = useContext(DataContext);
+  const { isRegisterModalOpen, setIsRegisterModalOpen, search, setSearch, user } = useContext(DataContext);
+  const [cookies, setCookie, removeCookie ] = useCookies(["user"]);
+  const navigate = useNavigate();
+  const handleSignOut = async () => {
+    removeCookie("user");
+    await userSignOut();
+    navigate("/");
+  }
   
   const handleInputFocus = (e:KeyboardEvent) => {
     const isK = e.key === "k";
@@ -37,7 +47,13 @@ const Header = () => {
         <label htmlFor="busca">Pesquisa</label>
         <input type="text" name="busca" id="busca" ref={input} value={search} onChange={(e) => setSearch(e.target.value)} />
       </div>
-      <button type="button" className="header__button" onClick={openModal}>Cadastrar usuário</button>
+      <div className="header__actions">
+        <div className="header__user-info">
+          <span className="header__user-name">{user?.email}</span>
+          <button onClick={handleSignOut} className="header__user-signout" type="button">Sair</button>
+        </div>
+        <button type="button" className="header__button" onClick={openModal}>Cadastrar usuário</button>
+      </div>
 
       {isRegisterModalOpen &&
         <CreateGuest />
