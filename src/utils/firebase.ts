e2509591efe13fca427ceb17437ae52b5f6bae2e
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-08-31 19:42:10 -0300

    Updated firebase | login / logout handlers

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,5 +1,10 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAdOdJBtVMM0VHzpIyDuYIuV_9h6GgJDe0",
@@ -7,8 +12,30 @@ const firebaseConfig = {
   projectId: "registrador-de-visitas",
   storageBucket: "registrador-de-visitas.appspot.com",
   messagingSenderId: "460062881217",
-  appId: "1:460062881217:web:615c31b48aecbef232ab40"
+  appId: "1:460062881217:web:615c31b48aecbef232ab40",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
+
+export const userRegistration = async ({ email, senha }: UserDataProps) => {
+  if (!email || !senha) return;
+
+  return await createUserWithEmailAndPassword(auth, email, senha);
+};
+
+export const userLogin = async ({ email, senha }: UserDataProps) => {
+  return await signInWithEmailAndPassword(auth, email, senha);
+};
+
+export const userSignOut = async () => {
+
+  return signOut(auth);
+}
+
+// Types
+
+interface UserDataProps {
+  email: string;
+  senha: string;
+}
