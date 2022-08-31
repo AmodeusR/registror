9434e58e737b24be8469eed2ce3647f2c83a955e
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-08-31 19:42:21 -0300

    Configured React Cookie

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { DataProvider } from "./contexts/data.context";
-import "./styles/main.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Login } from "./components";
+import { CookiesProvider } from "react-cookie";
+
+import "./styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <DataProvider>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/entrar" element={<Login />} />
-        </Routes>
-      </DataProvider>
-    </BrowserRouter>
+    <CookiesProvider>
+      <BrowserRouter>
+        <DataProvider>
+          <Routes>
+            <Route path="/inicio" element={<App />} />
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </DataProvider>
+      </BrowserRouter>
+    </CookiesProvider>
   </React.StrictMode>
 );
