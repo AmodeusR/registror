4a0b80ed365b07a97edda4aa025113619fcbe761
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-09-12 18:41:08 -0300

    set user if logged in & fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,63 @@
+import { User } from "firebase/auth";
 import { useContext, useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 import { Header, Sidebar, Main, GuestInfoCard } from "./components";
 import { GuestCardProps } from "./components/Cards/cards.types";
 import DataContext from "./contexts/data.context";
+import { fetchFirestoreData, onAuthStateChangedListener } from "./utils/firebase";
 
 function App() {
+  const { user, setUser } = useContext(DataContext);
   const [guest, setGuest] = useState({} as GuestCardProps);
+  const [ cookies ] = useCookies();
+  const navigate = useNavigate();
 
-  const { GuestCardInfoModalId } = useContext(DataContext);
+  const { GuestCardInfoModalId, fetchedGuests, setFetchedGuests } = useContext(DataContext);
   
   useEffect(() => {
-    const fetchGuests = async () => {
-      const guestsPromise = await fetch("http://localhost:3000/guests");
-      const guests = await guestsPromise.json();
-      
-      const selectedGuest = guests.find((guest: GuestCardProps) => guest.cpf === Number(GuestCardInfoModalId));
+    
+    if (!cookies.user) {
+      navigate("/");
+    } else {
+      setUser(cookies.user);
+    }    
+  }, []);
+  
+  useEffect(() => {
+    const fetchGuests = async () => {      
+      const selectedGuest = fetchedGuests.find((guest: GuestCardProps) => guest.cpf === GuestCardInfoModalId);
+
+      if (!selectedGuest) return;
 
       setGuest(selectedGuest);
     }
 
     fetchGuests();
   }, [GuestCardInfoModalId]);
+
+  useEffect(() => {
+    const fetchData = async (user: User) => {
+      const data = await fetchFirestoreData(user);
+
+      return data;
+    }
+
+    const unsubscribe = onAuthStateChangedListener(async (user) => {
+      setUser(user);
+
+      if (user) {
+        const data = await fetchData(user);
+
+        if (!data) return;
+        setFetchedGuests(data.guests);
+
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <div className="app">
       <Header />
