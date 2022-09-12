54b0a833faa9234757572746517509ef79a2dec8
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-09-12 18:30:22 -0300

    Update data.context.tsx
    GuestCardInfoModalId to Number & created fetchedGuests state

--- src/contexts/data.context.tsx
+++ src/contexts/data.context.tsx
@@ -1,19 +1,22 @@
 import { createContext, ReactNode, useState } from "react";
 import { User as FirebaseUser } from "firebase/auth";
+import { GuestCardProps } from "../components/Cards/cards.types";
 
 interface ContextType {
   selectedSidebarTab: null |"guests" | "visiting" | "history";
   setSelectedSidebarTab: (buttonClicked: selectionOptions) => void;
   isRegisterModalOpen: boolean;
   setIsRegisterModalOpen: (isOpen: boolean) => void;
-  GuestCardInfoModalId: string;
-  setGuestCardInfoModalId: (isOpen: string) => void;
+  GuestCardInfoModalId: number | null;
+  setGuestCardInfoModalId: (isOpen: number | null) => void;
   search: string;
   setSearch: (searchTerm: string) => void;
   isImageModalOpen: boolean;
   setIsImageModalOpen: (isModalOpen: boolean) => void;
   user: FirebaseUser | null;
   setUser: (user: FirebaseUser | null) => void;
+  fetchedGuests: GuestCardProps[];
+  setFetchedGuests: (guests: GuestCardProps[]) => void;
 }
 
 const DataContext = createContext({} as ContextType);
@@ -28,10 +31,12 @@ export type selectionOptions = null |"guests" | "visiting" | "history";
 export const DataProvider = ({ children }: DataProvider) => {
   const [selectedSidebarTab, setSelectedSidebarTab] = useState<selectionOptions>(null);
   const [isRegisterModalOpen, setIsRegisterModalOpen] = useState(false);
-  const [GuestCardInfoModalId, setGuestCardInfoModalId] = useState("");
+  const [GuestCardInfoModalId, setGuestCardInfoModalId] = useState<number | null>(null);
   const [search, setSearch] = useState("");
   const [isImageModalOpen, setIsImageModalOpen] = useState(false);
   const [user, setUser] = useState<FirebaseUser | null>(null);
+  const [fetchedGuests, setFetchedGuests] = useState<GuestCardProps[]>([]);
+
 
   return (
     <DataContext.Provider value={{
@@ -46,7 +51,9 @@ export const DataProvider = ({ children }: DataProvider) => {
       isImageModalOpen,
       setIsImageModalOpen,
       user,
-      setUser
+      setUser,
+      fetchedGuests,
+      setFetchedGuests
     }}>
       { children }
     </DataContext.Provider>
