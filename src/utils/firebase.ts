012e98c1a39d0ee4d9c2c85ac28a8680f8b664bd
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-09-12 18:28:42 -0300

    fixed fetch bug on app startup

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,10 +1,15 @@
+import { GuestCardProps } from "./../components/Cards/cards.types";
 import { initializeApp } from "firebase/app";
 import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  signOut
+  signOut,
+  User,
+  onAuthStateChanged,
+  NextOrObserver,
 } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc, updateDoc, arrayUnion } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAdOdJBtVMM0VHzpIyDuYIuV_9h6GgJDe0",
@@ -14,6 +19,8 @@ const firebaseConfig = {
   messagingSenderId: "460062881217",
   appId: "1:460062881217:web:615c31b48aecbef232ab40",
 };
+
+  // User Functions
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
@@ -29,8 +36,71 @@ export const userLogin = async ({ email, senha }: UserDataProps) => {
 };
 
 export const userSignOut = async () => {
-
   return signOut(auth);
+};
+
+export const onAuthStateChangedListener = (callback: NextOrObserver<User>) => {
+
+  return onAuthStateChanged(auth, callback)
+}
+
+
+// Firestore Functions
+
+const db = getFirestore();
+
+export const createUserDocument = async (user: User) => {
+  const userDocRef = doc(db, "users", user.uid);
+
+  const dataSnapshot = await getDoc(userDocRef);
+
+  if (!dataSnapshot.exists()) {
+    try {
+      await setDoc(userDocRef, {
+        guests: [],
+        visiting: [],
+        history: []
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }  
+}
+
+export const fetchFirestoreData = async (user: User | null) => {    
+  if(!user) return [];
+
+  const userDocRef = doc(db, "users", user.uid);
+  const dataSnapshot = await getDoc(userDocRef);
+  const data = dataSnapshot.data();  
+  
+  return data;
+}
+
+export const createNewGuest = async (newGuest: GuestCardProps) => {  
+  if (!auth.currentUser) return;
+
+  const userDocRef = doc(db, "users", auth.currentUser.uid);
+
+  const dataSnapshot = await getDoc(userDocRef);
+  const data = dataSnapshot.data()
+  const guestsCPF = data?.guests.map((guest: GuestCardProps) => guest.cpf);
+  
+  const isCPFRegistered = guestsCPF.some((guestCPF: number) => guestCPF === newGuest.cpf);
+
+  if (isCPFRegistered) {
+    return "Este CPF já está registrado";
+  }
+  
+  try {
+    await updateDoc(userDocRef, {
+      guests: arrayUnion(newGuest)
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  return null;
 }
 
 // Types
