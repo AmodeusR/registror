2390dea4cf53df6d22979bb1eca22785ef872630
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-09-13 12:18:31 -0300

    Created UpdateGuestPicture function

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -9,7 +9,16 @@ import {
   onAuthStateChanged,
   NextOrObserver,
 } from "firebase/auth";
-import { getFirestore, doc, getDoc, setDoc, updateDoc, arrayUnion } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  arrayUnion,
+  arrayRemove
+} from "firebase/firestore";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAdOdJBtVMM0VHzpIyDuYIuV_9h6GgJDe0",
@@ -20,10 +29,11 @@ const firebaseConfig = {
   appId: "1:460062881217:web:615c31b48aecbef232ab40",
 };
 
-  // User Functions
+// User Functions
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
+const storage = getStorage();
 
 export const userRegistration = async ({ email, senha }: UserDataProps) => {
   if (!email || !senha) return;
@@ -40,10 +50,8 @@ export const userSignOut = async () => {
 };
 
 export const onAuthStateChangedListener = (callback: NextOrObserver<User>) => {
-
-  return onAuthStateChanged(auth, callback)
-}
-
+  return onAuthStateChanged(auth, callback);
+};
 
 // Firestore Functions
 
@@ -59,53 +67,96 @@ export const createUserDocument = async (user: User) => {
       await setDoc(userDocRef, {
         guests: [],
         visiting: [],
-        history: []
+        history: [],
       });
     } catch (error) {
       console.log(error);
     }
-  }  
-}
+  }
+};
 
-export const fetchFirestoreData = async (user: User | null) => {    
-  if(!user) return [];
+export const fetchFirestoreData = async () => {
+  const currentUser = auth.currentUser;
+  if (!currentUser) return [];
 
-  const userDocRef = doc(db, "users", user.uid);
+  const userDocRef = doc(db, "users", currentUser.uid);
   const dataSnapshot = await getDoc(userDocRef);
-  const data = dataSnapshot.data();  
-  
+  const data = dataSnapshot.data();
+
   return data;
-}
+};
 
-export const createNewGuest = async (newGuest: GuestCardProps) => {  
-  if (!auth.currentUser) return;
+export const createNewGuest = async (newGuest: GuestCardProps) => {
+  const currentUser = auth.currentUser;
+  if (!currentUser) return;
 
-  const userDocRef = doc(db, "users", auth.currentUser.uid);
+  const userDocRef = doc(db, "users", currentUser.uid);
 
   const dataSnapshot = await getDoc(userDocRef);
-  const data = dataSnapshot.data()
+  const data = dataSnapshot.data();
   const guestsCPF = data?.guests.map((guest: GuestCardProps) => guest.cpf);
-  
-  const isCPFRegistered = guestsCPF.some((guestCPF: number) => guestCPF === newGuest.cpf);
+  const isCPFRegistered = guestsCPF.some(
+    (guestCPF: number) => guestCPF === newGuest.cpf
+  );
 
   if (isCPFRegistered) {
     return "Este CPF já está registrado";
   }
-  
   try {
     await updateDoc(userDocRef, {
-      guests: arrayUnion(newGuest)
+      guests: arrayUnion(newGuest),
     });
   } catch (error) {
     console.log(error);
   }
 
   return null;
+};
+
+export const updateGuestPicture = async (guestToUpdate: GuestCardProps) => {
+  const currentUser = auth.currentUser;
+  if (!currentUser) return;
+
+  const userDocRef = doc(db, "users", currentUser.uid);
+
+  const dataSnapshot = await getDoc(userDocRef);
+  const data = dataSnapshot.data();
+  const guests = data?.guests.filter((guest: GuestCardProps) => guest.cpf !== guestToUpdate.cpf);
+    
+  try {
+    await updateDoc(userDocRef, {
+      guests: [...guests, guestToUpdate]
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+// Storage Functions
+
+  export const uploadUserPicture = ({img, userID}: UploadDataProps) => {
+    const userPictureRef = ref(storage, `userPictures/${userID}`);
+
+    try {
+      const imgLink = uploadBytes(userPictureRef, img)
+        .then(snapshot => snapshot.ref)
+        .then(snapshotRef => getDownloadURL(snapshotRef));
+
+        return imgLink;
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
 
 // Types
 
 interface UserDataProps {
   email: string;
   senha: string;
+}
+
+interface UploadDataProps {
+  img: Blob;
+  userID: number;
 }
