38e94eb371ba59f6b4f54d307456d05083ac32ad
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-09-24 16:03:15 -0300

    VisitorInfoCard folder index

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,5 +4,6 @@ export { default as Main } from "./Main/Main";
 export { default as CreateGuest } from "./CreateGuest/CreateGuest"; 
 export { default as InputLabel } from "./InputLabel/InputLabel"; 
 export { default as GuestInfoCard } from "./GuestInfoCard/GuestInfoCard"; 
+export { default as VisitorInfoCard } from "./VisitorInfoCard/VisitorInfoCard"; 
 export { default as ImageShooterModal } from "./ImageShooterModal/ImageShooterModal"; 
 export { default as Login } from "./Login/Login"; 
