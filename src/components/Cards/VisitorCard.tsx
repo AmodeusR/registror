f8f5166f3ef5c718c811486339d7dbdec786afa2
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-09-24 16:04:11 -0300

    Update VisitorCard.tsx

--- src/components/Cards/VisitorCard.tsx
+++ src/components/Cards/VisitorCard.tsx
@@ -1,37 +1,59 @@
-import "./card.scss";
+import { formatAddress } from "../../utils/formatAddress";
+import { formatCPF } from "../../utils/formatCPF";
+import { VisitorCardProps } from "./cards.types";
+import ProfilePlaceholder from "../../assets/profile-placeholder.webp";
+import { formatDateRelative, formatDate } from "../../utils/formatDate";
+import { useContext } from "react";
+import DataContext from "../../contexts/data.context";
 
-const VisitorCard = () => {
+const VisitorCard = ({
+  guestPicture,
+  nome,
+  cpf,
+  cidade,
+  bairro,
+  rua,
+  numero,
+  complemento,
+  entrada
+}: VisitorCardProps) => {
+  const { setVisitorCardInfoModalId } = useContext(DataContext);
+
+  const handleClick = () => {
+    setVisitorCardInfoModalId(cpf);
+  }
+
   return (
-    <button className="card">
+    <button className="card" onClick={handleClick}>
       <img
         className="card__image"
-        src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+        src={guestPicture || ProfilePlaceholder}
         alt="imagem aleatória"
       />
       <div className="card__section">
         <span className="card__label">nome</span>
-        <p className="card__info">André Aquino de Bragança</p>
+        <p className="card__info">{nome}</p>
       </div>
 
       <div className="card__vertical-line" />
 
       <div className="card__section">
         <span className="card__label">cpf</span>
-        <p className="card__info">123.456.789-0</p>
+        <p className="card__info">{formatCPF(cpf)}</p>
       </div>
 
       <div className="card__vertical-line" />
 
       <div className="card__section">
         <span className="card__label">visitando</span>
-        <p className="card__info">Av. Alameda, 222 — Fortaleza</p>
+        <p className="card__info">{formatAddress({cidade, rua, numero})}</p>
       </div>
 
       <div className="card__vertical-line" />
 
       <div className="card__section">
         <span className="card__label">entrada</span>
-        <p className="card__info">Domingo, às 14h16</p>
+        <p className="card__info">{formatDateRelative(entrada)}</p>
       </div>
     </button>
   );
