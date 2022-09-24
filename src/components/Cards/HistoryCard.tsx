6cc81c3ba5f34743abf29c28b0ffcc00c8d55ce6
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-09-24 16:04:18 -0300

    Update HistoryCard.tsx

--- src/components/Cards/HistoryCard.tsx
+++ src/components/Cards/HistoryCard.tsx
@@ -1,44 +1,60 @@
 import "./card.scss";
+import { HistoryCardProps } from "./cards.types";
+import ProfilePlaceholder from "../../assets/profile-placeholder.webp";
+import { formatCPF } from "../../utils/formatCPF";
+import { formatAddress } from "../../utils/formatAddress";
+import { formatDateRelative } from "../../utils/formatDate";
 
-const VisitorCard = () => {
+const VisitorCard = ({
+  guestPicture,
+  nome,
+  cpf,
+  cidade,
+  bairro,
+  rua,
+  numero,
+  complemento,
+  entrada,
+  saida
+}: HistoryCardProps) => {
   return (
     <button className="card">
       <img
         className="card__image"
-        src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+        src={guestPicture || ProfilePlaceholder}
         alt="imagem aleatória"
       />
       <div className="card__section">
         <span className="card__label">nome</span>
-        <p className="card__info">André Aquino de Bragança</p>
+        <p className="card__info">{nome}</p>
       </div>
 
       <div className="card__vertical-line" />
 
       <div className="card__section">
         <span className="card__label">cpf</span>
-        <p className="card__info">123.456.789-0</p>
+        <p className="card__info">{formatCPF(cpf)}</p>
       </div>
 
       <div className="card__vertical-line" />
 
       <div className="card__section">
         <span className="card__label">visitando</span>
-        <p className="card__info">Av. Alameda, 222 — Fortaleza</p>
+        <p className="card__info">{formatAddress({cidade, rua, numero})}</p>
       </div>
 
       <div className="card__vertical-line" />
 
       <div className="card__section">
         <span className="card__label">entrada</span>
-        <p className="card__info">Domingo, às 14h16</p>
+        <p className="card__info">{formatDateRelative(entrada)}</p>
       </div>
 
       <div className="card__vertical-line" />
 
       <div className="card__section">
         <span className="card__label">saída</span>
-        <p className="card__info">Domingo, às 16h00</p>
+        <p className="card__info">{formatDateRelative(saida)}</p>
       </div>
     </button>
   );
