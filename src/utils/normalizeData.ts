5a841435d616ffe9768a66c57f5a934ab19edb4c
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-11-16 00:30:03 -0300

    Update normalizeData.ts

--- src/utils/normalizeData.ts
+++ src/utils/normalizeData.ts
@@ -1,15 +1,27 @@
-import { DocumentData } from "firebase/firestore";
+import { DocumentData, QuerySnapshot } from "firebase/firestore";
 
-type CurrentArrayProps = {
-  data: object[];
+type NormalizeDataReturn = {
+  normalizedData: DocumentData;
+  mappedDocs?: DocumentData[] | null;
 }
 
-export const normalizeData = (dataToNormalize: DocumentData): DocumentData => {
+export const normalizeData = (collectionSnapshot: QuerySnapshot<DocumentData>, getMappedDocs = false): DocumentData | NormalizeDataReturn => {
+  const dataToNormalize = collectionSnapshot.docs.map(coll => coll.data());
 
-  const normalizedData = dataToNormalize.reduce((acc: object[], currentArray: CurrentArrayProps) => {
+  const normalizedData = dataToNormalize.reduce((acc: object[], currentArray) => {
     
-    return [...acc, ...currentArray.data]  
+    return [...acc, ...currentArray.data];
   }, []);
+  
+  
+  if (getMappedDocs) {
+    const mappedDocs = getMappedDocs ? dataToNormalize : null;
+    return {
+      normalizedData,
+      mappedDocs
+    };
+  }
 
   return normalizedData;
+  
 }
